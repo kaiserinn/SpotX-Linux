539c9a64740b89ab418475e84b04b9891c16a894
kaiserinn <azharizqullah90@gmail.com>
2023-12-04 18:19:47 +0800

    changed xpui.sa maximum size search

--- install.sh
+++ install.sh
@@ -133,7 +133,7 @@ if [ -z ${INSTALL_PATH+x} ]; then
         echo "Spotify directory found in PATH: ${INSTALL_PATH}"
     elif [[ ! -d ${INSTALL_PATH} ]]; then
         echo -e "\nSpotify not found in PATH. Searching for Spotify directory..."
-        INSTALL_PATH=$(timeout 10 find / -type f -path "*/spotify*Apps/*" -name "xpui.spa" -size -7M -size +3M -print -quit 2> /dev/null | rev | cut -d/ -f3- | rev)
+        INSTALL_PATH=$(timeout 10 find / -type f -path "*/spotify*Apps/*" -name "xpui.spa" -size -10M -size +3M -print -quit 2> /dev/null | rev | cut -d/ -f3- | rev)
         if [[ -d ${INSTALL_PATH} ]]; then
             echo "Spotify directory found: ${INSTALL_PATH}"
         elif [[ ! -d ${INSTALL_PATH} ]]; then
@@ -142,7 +142,7 @@ if [ -z ${INSTALL_PATH+x} ]; then
         fi
     elif [[ ${INSTALL_PATH} == "/usr/bin" ]]; then
         echo -e "\nSpotify PATH is set to /usr/bin, searching for Spotify directory..."
-        INSTALL_PATH=$(timeout 10 find / -type f -path "*/spotify*Apps/*" -name "xpui.spa" -size -7M -size +3M -print -quit 2> /dev/null | rev | cut -d/ -f3- | rev)
+        INSTALL_PATH=$(timeout 10 find / -type f -path "*/spotify*Apps/*" -name "xpui.spa" -size -10M -size +3M -print -quit 2> /dev/null | rev | cut -d/ -f3- | rev)
         if [[ -d ${INSTALL_PATH} && ${INSTALL_PATH} != "/usr/bin" ]]; then
             echo "Spotify directory found: ${INSTALL_PATH}"
         elif [[ ${INSTALL_PATH} == "/usr/bin" ]] || [[ ! -d ${INSTALL_PATH} ]]; then
